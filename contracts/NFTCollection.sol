1fb37572352f753f4c0028ec0f8690318476e8fa
imkyle94 <andy3638@naver.com>
2022-06-01 00:40:31 +0900

    contracts update

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -1,52 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 
-contract NFTCollection is ERC721, ERC721Enumerable {
-    string[] public tokenURIs;
-    mapping(string => bool) _tokenURIExists;
-    mapping(uint256 => string) _tokenIdToTokenURI;
+contract NFTCollection is ERC1155Supply {
+  string[] public tokenURIs;
+  mapping(string => bool) _tokenURIExists;
+  mapping(uint256 => string) _tokenIdToTokenURI;
 
-    constructor() ERC721("mTC Collection", "mTC") {}
+  constructor() ERC1155("NFT") {}
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
+  // function tokenURI(uint256 tokenId)
+  //     public
+  //     view
+  //     override
+  //     returns (string memory)
+  // {
+  //     require(
+  //         exists(tokenId),
+  //         "ERC721Metadata: URI query for nonexistent token"
+  //     );
+  //     return _tokenIdToTokenURI[tokenId];
+  // }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  function safeMint(string memory _tokenURI) public {
+    require(!_tokenURIExists[_tokenURI], "The token URI should be unique");
+    tokenURIs.push(_tokenURI);
+    uint256 _id = tokenURIs.length;
+    _tokenIdToTokenURI[_id] = _tokenURI;
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override
-        returns (string memory)
-    {
-        require(
-            _exists(tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
-        return _tokenIdToTokenURI[tokenId];
-    }
+    _mint(msg.sender, _id, 1, "1");
 
-    function safeMint(string memory _tokenURI) public {
-        require(!_tokenURIExists[_tokenURI], "The token URI should be unique");
-        tokenURIs.push(_tokenURI);
-        uint256 _id = tokenURIs.length;
-        _tokenIdToTokenURI[_id] = _tokenURI;
-        _safeMint(msg.sender, _id);
-        _tokenURIExists[_tokenURI] = true;
-    }
+    _tokenURIExists[_tokenURI] = true;
+  }
 }
