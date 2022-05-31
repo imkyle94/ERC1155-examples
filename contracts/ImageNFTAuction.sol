0da44ffeafbed7d42515ca1ffdea24899b813af9
ParkJungho <andy3638@naver.com>
2022-05-31 17:12:29 +0900

    contracts update

--- contracts/ImageNFTAuction.sol
+++ contracts/ImageNFTAuction.sol
@@ -4,100 +4,94 @@ pragma solidity ^0.8.0;
 import "./ImageNFT.sol";
 
 contract ImageAuction is ImageNFT {
-    struct Auction {
-        uint256 imageID;
-        uint256 startBid;
-        uint256 highestBid;
-        address payable winner;
-        uint256 endTime;
-        bool ended;
-        bool claimed;
-    }
+  struct Auction {
+    uint256 imageID;
+    uint256 startBid;
+    uint256 highestBid;
+    address payable winner;
+    uint256 endTime;
+    bool ended;
+    bool claimed;
+  }
 
-    mapping(uint256 => Auction) public auctions;
-    mapping(uint256 => mapping(address => uint256)) internal bidInfo;
+  mapping(uint256 => Auction) public auctions;
+  mapping(uint256 => mapping(address => uint256)) internal bidInfo;
 
-    constructor() {}
+  constructor() {}
 
-    modifier notOnBid(uint256 _tokenID) {
-        require(
-            imageStorage[_tokenID].status == Status.OffBid,
-            "Already on auction."
-        );
-        _;
-    }
+  modifier notOnBid(uint256 _tokenID) {
+    require(
+      imageStorage[_tokenID].status == Status.OffBid,
+      "Already on auction."
+    );
+    _;
+  }
 
-    function beginAuction(
-        uint256 _tokenID,
-        uint256 _minBid,
-        uint256 _duration
-    ) external notOnBid(_tokenID) returns (bool success) {
-        require(
-            ownerOf(_tokenID) == msg.sender,
-            "Only Owner Can Begin a Auction."
-        );
+  function beginAuction(
+    uint256 _tokenID,
+    uint256 _minBid,
+    uint256 _duration
+  ) external notOnBid(_tokenID) returns (bool success) {
+    require(ownerOf(_tokenID) == msg.sender, "Only Owner Can Begin a Auction.");
 
-        updateStatus(_tokenID, Status.OnBid);
-        updatePrice(_tokenID, _minBid);
+    updateStatus(_tokenID, Status.OnBid);
+    updatePrice(_tokenID, _minBid);
 
-        uint256 _endTime = block.timestamp + _duration;
+    uint256 _endTime = block.timestamp + _duration;
 
-        Auction memory newAuction = Auction(
-            _tokenID,
-            _minBid,
-            _minBid,
-            payable(msg.sender),
-            _endTime,
-            false,
-            false
-        );
+    Auction memory newAuction = Auction(
+      _tokenID,
+      _minBid,
+      _minBid,
+      payable(msg.sender),
+      _endTime,
+      false,
+      false
+    );
 
-        auctions[_tokenID] = newAuction;
-        return true;
-    }
+    auctions[_tokenID] = newAuction;
+    return true;
+  }
 
-    function bid(uint256 auctionID, uint256 newBid)
-        external
-        returns (bool success)
-    {
-        Auction storage auction = auctions[auctionID];
-        require(!auction.ended, "Auction already ended.");
-        require(newBid > auction.highestBid, "Lower bid? Joking.");
+  function bid(uint256 auctionID, uint256 newBid)
+    external
+    returns (bool success)
+  {
+    Auction storage auction = auctions[auctionID];
+    require(!auction.ended, "Auction already ended.");
+    require(newBid > auction.highestBid, "Lower bid? Joking.");
 
-        updatePrice(auction.imageID, newBid);
+    updatePrice(auction.imageID, newBid);
 
-        auction.winner = payable(msg.sender);
-        auction.highestBid = newBid;
+    auction.winner = payable(msg.sender);
+    auction.highestBid = newBid;
 
-        bidInfo[auctionID][msg.sender] = newBid;
-        return true;
-    }
+    bidInfo[auctionID][msg.sender] = newBid;
+    return true;
+  }
 
-    function endAuction(uint256 auctionID) external returns (bool success) {
-        require(
-            ownerOf(auctionID) == msg.sender,
-            "Only Owner Can End a Auction."
-        );
+  function endAuction(uint256 auctionID) external returns (bool success) {
+    require(ownerOf(auctionID) == msg.sender, "Only Owner Can End a Auction.");
 
-        Auction storage auction = auctions[auctionID];
-        require(block.timestamp >= auction.endTime, "Not end time.");
-        require(!auction.ended, "Already Ended.");
+    Auction storage auction = auctions[auctionID];
+    require(block.timestamp >= auction.endTime, "Not end time.");
+    require(!auction.ended, "Already Ended.");
 
-        updateStatus(auction.imageID, Status.WaittingClaim);
-        auction.ended = true;
-        return true;
-    }
+    updateStatus(auction.imageID, Status.WaittingClaim);
+    auction.ended = true;
+    return true;
+  }
 
-    function claim(uint256 auctionID) external payable {
-        Auction storage auction = auctions[auctionID];
+  function claim(uint256 auctionID) external payable {
+    Auction storage auction = auctions[auctionID];
 
-        require(auction.ended, "Auction not ended yet.");
-        require(!auction.claimed, "Auction already claimed.");
-        require(auction.winner == msg.sender, "Can only be claimed by winner.");
+    require(auction.ended, "Auction not ended yet.");
+    require(!auction.claimed, "Auction already claimed.");
+    require(auction.winner == msg.sender, "Can only be claimed by winner.");
 
-        address owner = ownerOf(auction.imageID);
-        payable(owner).transfer(msg.value);
-        updateOwner(auction.imageID, msg.sender);
-        updateStatus(auction.imageID, Status.OffBid);
-    }
+    address owner = ownerOf(auction.imageID);
+    payable(owner).transfer(msg.value);
+    updateOwner(auction.imageID, msg.sender);
+    updateStatus(auction.imageID, Status.OffBid);
+  }
 }
